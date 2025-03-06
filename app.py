1473767560846c90896e6c3321f46972874337aa
Clase23 <149850204+Clase23@users.noreply.github.com>
2025-03-05 18:14:58 -0600

    Update app.py

--- app.py
+++ app.py
@@ -6,32 +6,23 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
  
-@app.route('/predict-emotion', methods=["POST"])
-def predict_emotion():
+#@app.route('/predict-emotion', methods=["POST"])
+#def predict_emotion():
     
     # Obtener el texto ingresado del requerimiento POST.
-    input_text = request.json.get("text")  
+   
     
-    if not input_text:
+    #if not input_text:
         # Respuesta para enviar si input_text está indefinido.
-        response = {
-                    "status": "error",
-                    "message": "¡Por favor, ingresa algún texto para predecir la emoción!"
-                  }
-        return jsonify(response)
-    else:  
-        predicted_emotion,predicted_emotion_img_url = predict(input_text)
+       
         
         # Respuesta para enviar si input_text no está indefinido.
-        response = {
-                    "status": "success",
-                    "data": {
-                            "predicted_emotion": predicted_emotion,
-                            "predicted_emotion_img_url": predicted_emotion_img_url
-                            }  
-                   }
-
+        
         # Enviar respuesta.         
-        return jsonify(response)
+        
        
-app.run(debug=True)  
+app.run(debug=True)
+
+
+
+#https://github.com/NellyGilA/PRO-C117-Plantilla-Codigo  
